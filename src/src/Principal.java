447bac5eac019490312dfeab2d0141b1b82e79eb
Alexandre Barbosa <alexandre.cbar@gmail.com>
2020-01-05 17:07:50 -0300

    Criação das classes 'Peca' e 'Tabuleiro'

--- src/src/Principal.java
+++ src/src/Principal.java
@@ -1,13 +1,15 @@
 package src;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		Posicao xy = new Posicao(2,7);
-		System.out.println(xy);
+//		Posicao xy = new Posicao(2,7);
+//		System.out.println(xy);
+		
+		Tabuleiro tabuleiro = new Tabuleiro(8,8);
 		
 	}
 
