9647bc5b4d07202ee1616d28485badcf4adabaa0
Alexandre Barbosa <alexandre.cbar@gmail.com>
2020-01-05 14:05:34 -0300

    Criação da classe 'Posicao'.

---
+++ src/src/Principal.java
@@ -0,0 +1,14 @@
+package src;
+
+import tabuleiro.Posicao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Posicao xy = new Posicao(2,7);
+		System.out.println(xy);
+		
+	}
+
+}
